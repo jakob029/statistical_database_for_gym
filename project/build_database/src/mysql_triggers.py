8716513e102fd93c569f48edd586ac18d312f567
FilipDar <152434723+FilipDar@users.noreply.github.com>
2024-05-19 11:27:28 +0200

    Trigger complete
    Trigger to update wear and tear

--- project/build_database/src/mysql_triggers.py
+++ project/build_database/src/mysql_triggers.py
@@ -2,13 +2,13 @@
 - Instead update wear and tear score.
 """
 
-wear_and_tear_trigger = """
-DELIMITER //
+SQL_Triggers"""
 
-CREATE TRIGGER update_wear_and_tear
-AFTER UPDATE
-ON Machine_stats_table
+CREATE TRIGGER test AFTER UPDATE ON machine_stats_table
 FOR EACH ROW
+    BEGIN
+        update machine_table
+        SET Wear_tear_index = (new.Total_rep_count / 100 + DATEDIFF(CURDATE(),installment_date)) / 1000
+        WHERE MachineID = NEW.MachineID;
+    end $$
 """
-
-## FINISH THIS BY UPDATING THE WEAR AND TEAR INDEX BASED ON REPS AND INSTALLMENT DATE
