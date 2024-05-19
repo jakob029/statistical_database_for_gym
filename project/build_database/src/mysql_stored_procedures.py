a274c7307d9d082e11438f442037e2ffe3b2c64e
jakob029 <jacke029@gmail.com>
2024-05-19 18:24:13 +0200

    New procedure

--- project/build_database/src/mysql_stored_procedures.py
+++ project/build_database/src/mysql_stored_procedures.py
@@ -4,7 +4,7 @@
 """
 
 mysql_stored_procedures = """
-CREATE PROCEDURE InsertSet (IN MachID int, IN MemID int, IN Reps INT, IN Weight INT)
+CREATE PROCEDURE insert_machine_set (IN MachID int, IN MemID int, IN Reps INT, IN Weight INT)
 BEGIN
     DECLARE new_score INT;
     SET new_score = round(Weight / EXP(-0.023 * (Reps - 1)));
@@ -20,6 +20,23 @@ BEGIN
     ELSE
         INSERT INTO Machine_stats_table(MachineID, MemberID, Total_rep_count, High_score)
         VALUES (MachID, MemID, Reps, new_score);
+    END IF;
+END;
+
+
+CREATE PROCEDURE insert_exercise_set (IN exercise varchar(255), IN MemID int, IN Reps INT, IN Weight INT)
+BEGIN
+    DECLARE new_score INT;
+    SET new_score = round(Weight / EXP(-0.023 * (Reps - 1)));
+
+    if (Select count(*) from Exercise_score_table e where
+    exercise = e.Exercise and MemID = MemberID) > 0
+    THEN
+        UPDATE Exercise_score_table e SET High_score = GREATEST(High_score, new_score)
+        where e.exercise = Exercise and MemID = MemberID;
+    ELSE
+        INSERT INTO Exercise_score_table(Exercise, MemberID, High_score)
+        VALUES (exercise, MemID, new_score);
     END IF;
 END;
 """
